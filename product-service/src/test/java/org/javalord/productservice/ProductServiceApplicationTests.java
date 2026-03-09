be7ed2a1f4a6b4be51b18a7f4fae9a2bca67a2f5
Christopher-OOC <olojedechristopher24@gmail.com>
2026-03-09 10:51:39 +0100

    commit

--- product-service/src/test/java/org/javalord/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/org/javalord/productservice/ProductServiceApplicationTests.java
@@ -1,15 +1,61 @@
 package org.javalord.productservice;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mongodb.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp()
+    {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
     @Test
-    void contextLoads() {
+    void shouldCreateProduct()
+    {
+        RestAssured.baseURI = "http://localhost";
+        String requestBody = """
+                {
+                    "name": "IPhone 15",
+                    "description": "IPhone 15",
+                    "price": 15
+                }
+                """;
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/api/products")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.is("IPhone 15"))
+                .body("description", Matchers.is("IPhone 15"))
+                .body("price", Matchers.is(15));
+
     }
 
 }
