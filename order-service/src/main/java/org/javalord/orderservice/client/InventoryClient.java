c5585ebcb33ae3e48f6c362094d5075421dc1327
Christopher-OOC <olojedechristopher24@gmail.com>
2026-03-17 19:07:21 +0100

    cpmit

--- order-service/src/main/java/org/javalord/orderservice/client/InventoryClient.java
+++ order-service/src/main/java/org/javalord/orderservice/client/InventoryClient.java
@@ -1,16 +1,14 @@
 package org.javalord.orderservice.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.PostExchange;
 
-@FeignClient(name = "inventory", url = "http://inventory-service:8003")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/api/v1/inventory")
-        boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
+    @PostExchange(value = "/api/v1/inventory")
+    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
 
 }
